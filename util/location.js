05dd2f9c5817e95eee76f0729ecbed4f3f5ffe74
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-01 18:01:02 +0000

    defined a function to return human readable address using google geocoding api

--- util/location.js
+++ util/location.js
@@ -1,7 +1,21 @@
-const GOOGLE_MAP_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY;
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
-&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_MAP_API_KEY}`;
+&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
   return imagePreviewUrl;
+}
+
+export async function getAddress(lat, lng) {
+  console.log('called');
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`;
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch address!');
+  }
+  const data = await res.json();
+  console.log(data.results[0].formatted_address);
+  const address = data.results[0].formatted_address;
+  return address;
 }
