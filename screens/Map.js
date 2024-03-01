93e4ce4f9f7fc1eaae7aa74afac9371770ffb601
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-01 15:05:44 +0000

    configured map interaction and markers

--- screens/Map.js
+++ screens/Map.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
+  const [selectedLocation, setSelectLocation] = useState();
   const region = {
     latitude: 37.78,
     latitudeDelta: 0.0922,
     longitude: -122.43,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectLocation({ lat, lng });
+  }
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title='Picked Location'
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 export default Map;
