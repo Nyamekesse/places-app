92a647884955abc17fb781c4d942d7a4b8098525
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-02 22:00:21 +0000

    called fetch all places in useEffect hook

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -2,15 +2,20 @@ import { useIsFocused } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import PlacesList from '../components/Places/PlacesList';
+import { fetchPlaces } from '../util/database';
 
-const AllPlaces = ({ route }) => {
+const AllPlaces = () => {
   const isFocused = useIsFocused();
   const [loadedPlaces, setLoadedPlaces] = useState([]);
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((currPlaces) => [...currPlaces, route.params.place]);
+    async function loadPlaces() {
+      await fetchPlaces();
     }
-  }, [isFocused, route]);
+    if (isFocused) {
+      loadPlaces();
+      // setLoadedPlaces((currPlaces) => [...currPlaces, route.params.place]);
+    }
+  }, [isFocused]);
   return <PlacesList places={loadedPlaces} />;
 };
 
