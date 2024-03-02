61dfd24ce90ba8db4bb750b7447be38b4b80dc11
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-03-02 16:29:10 +0000

    passed route parameter places to PlaceList component

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import PlacesList from '../components/Places/PlacesList';
 
-const AllPlaces = () => {
-  return <PlacesList />;
+const AllPlaces = ({ route }) => {
+  const isFocused = useIsFocused();
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((currPlaces) => [...currPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+  return <PlacesList places={loadedPlaces} />;
 };
 
 export default AllPlaces;
